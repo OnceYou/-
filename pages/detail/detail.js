a42d76e6f86f7cd5574720c55dad5b867a1b73d3
once <389460935@qq.com>
2018-12-14 17:56:39 +0800

    优化详情页

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -12,7 +12,6 @@ Page({
   onLoad: function (options) {
     var date = options.date;
     var e_id = options.e_id;
-    console.log(date);
     var domain = app.globalData.domain;
     var url = domain + '/todayOnhistory/queryDetail.php?e_id=' + e_id + '&key=253b98a10624a1e96fec6e767bd9cd7d';
     common.http(url, 'GET', this.callbackDate);
@@ -24,13 +23,26 @@ Page({
   callbackDate: function(data) {
     console.log(data);
     if(data.reason == 'success') {
+      var content_trim_arr = [];
       var content_arr = data.result[0].content.split('\r\n');
       console.log(content_arr);
+      for(let a = 0, len = content_arr.length; a < len; a++) {
+        if(content_arr[a] == '') {
+          continue;
+        }
+        content_trim_arr.push(this.trimSpace(content_arr[a]));
+      }
+      console.log(content_trim_arr);
       this.setData({
         results: data.result[0],
-        content: content_arr
+        content: content_trim_arr
       });
     }
+  },
+
+  trimSpace: function (str) {  
+    var result = str.replace(/(^\s+)|(\s+$)/g,"").replace(/\s/g,"");
+    return result;
   },
 
   onShareAppMessage: function (options) {
